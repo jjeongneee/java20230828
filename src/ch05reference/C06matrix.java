73dd9bf5a1e02d43f4c5f5302e66ead34e3f864c
jjeongneee <gardening0530@gmail.com>
2023-08-28 12:35:54 +0900

    오전에 공부한 내용

--- src/ch05reference/C06matrix.java
+++ src/ch05reference/C06matrix.java
@@ -21,7 +21,8 @@ public class C06matrix {
 		System.out.println(b[2][2]); // 333
 		System.out.println(a[2][2]); // 333
 		
-		b = new int[][] {{33, 44}, {22, 11}};
+		// b에 새객체(instance)의 참조값 할당됨
+		b = new int[][] {{33, 44}, {22, 11}}; // 여기부턴 a와 b가 서로 별개의 객체가 됨.
 		System.out.println(b[0][0]); //33
 		System.out.println(a[0][0]); //33? 1? -> 1
 	}
